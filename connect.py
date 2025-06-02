ca706d9ea65c2fafb24d75a72fa1450a5fadf63e
Simranpreet-Kaur-tech <119108495+Simranpreet-Kaur-tech@users.noreply.github.com>
2025-06-02 17:05:40 +1200

    update

--- connect.py
+++ connect.py
@@ -1,5 +1,5 @@
 dbuser = "root"  # PUT YOUR MySQL username here - usually admin
 dbpass = "pNNRrRMAwvcKTOiJwQjLxDIcYIcKvPEK"  # "PUT YOUR PASSWORD HERE"
-dbhost = "mysql.railway.internal"  # "PUT YOUR AWS Connect String here"
-dbport = "3306"
+dbhost = "yamabiko.proxy.rlwy.net"  # "PUT YOUR AWS Connect String here"
+dbport = "16533"
 dbname = "railway"
